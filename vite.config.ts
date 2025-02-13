c496a46a802c6840e676414248b557b19907b8d9
Lucas Amsellem <lucas.amsellem21@gmail.com>
2025-02-13 11:01:03 +0100

    Mise à jour du projet React

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/sell-deuche',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
